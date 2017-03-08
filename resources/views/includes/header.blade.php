55d4be39d6648232e48656e8460f1852cff825b1
Andrei Cojocaru <cojocaruandrei96@gmail.com>
2017-03-08 22:29:39 +0200

    first commit

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,13 +1,16 @@
 <header>
-    <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+        <!--<a class="navbar-brand" href="#">
+            <img src="/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
+        </a>-->
         <a class="navbar-brand" href="{{route('dashboard')}}">Socialize</a>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto mr-lg-0 ml-auto">
-                <img src="{{ route('account.image', ['filename' => $user->first_name . '-' . $user->id . '.jpg']) }}" alt="" class="img-responsive" id="account-picture"">
                 <li class="nav-item active"><a class="nav-link" href="{{route('account')}}">Accout <span class="sr-only">(current)</span></a></li>
                 <li class="nav-item active"><a class="nav-link" href="{{route('logout')}}">Logout <span class="sr-only">(current)</span></a></li>
             </ul>
